b532060bb33ca7c8cff7f3e1183642f94c37c894
dmuchai <dmmuchai@gmail.com>
2025-05-13 21:51:47 +0300

    Task 2 adding css

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -8,7 +8,7 @@ $('body').append('<p>Dashboard data for the students</p>');
 
 const button = $('<button>Click here to get started</button>');
 $('body').append(button);
-$('body').append('<p id="count"></p>');
+$('body').append('<p id="count">0 clicks on the button</p>'); // ✅ Initial value
 $('body').append('<p>Copyright - ALX</p>');
 
 let count = 0;
