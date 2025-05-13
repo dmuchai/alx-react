2a6b4e5e44fa8877cd0a3494066c7b53e32d4196
dmuchai <dmmuchai@gmail.com>
2025-05-13 22:26:05 +0300

    Task 2 css updates

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,17 +1,14 @@
 import $ from 'jquery';
 import _ from 'lodash';
 import '../css/main.css';
-import logo from '../assets/holberton-logo.jpg'; // Imports image
 
 $('body').append('<div id="logo"></div>');
-$('#logo').css('background', `url(${logo}) no-repeat center center`); // ✅ Set with JS
-
 $('body').append('<p>ALX Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 
 const button = $('<button>Click here to get started</button>');
 $('body').append(button);
-$('body').append('<p id="count">0 clicks on the button</p>');
+$('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - ALX</p>');
 
 let count = 0;
--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,13 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
-    clean: true,
   },
-  mode: 'production',
   module: {
     rules: [
       {
@@ -18,9 +17,20 @@ module.exports = {
         test: /\.(png|jpe?g|gif)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'assets/[name][ext]',
+          filename: 'images/[name][ext]'
         },
-      },
-    ],
-  },
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: { progressive: true },
+              optipng: { enabled: true },
+              pngquant: { quality: [0.65, 0.90], speed: 4 },
+              gifsicle: { interlaced: false },
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
