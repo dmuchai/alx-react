1b27744ad7cc9be00167a79e9db7e24f15d7ecd8
dmuchai <dmmuchai@gmail.com>
2025-05-13 22:05:38 +0300

    Task 2 vss updates

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,14 +1,17 @@
 import $ from 'jquery';
 import _ from 'lodash';
 import '../css/main.css';
+import logo from '../assets/holberton-logo.jpg'; // Imports image
 
 $('body').append('<div id="logo"></div>');
+$('#logo').css('background', `url(${logo}) no-repeat center center`); // ✅ Set with JS
+
 $('body').append('<p>ALX Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 
 const button = $('<button>Click here to get started</button>');
 $('body').append(button);
-$('body').append('<p id="count">0 clicks on the button</p>'); // ✅ Initial value
+$('body').append('<p id="count">0 clicks on the button</p>');
 $('body').append('<p>Copyright - ALX</p>');
 
 let count = 0;
