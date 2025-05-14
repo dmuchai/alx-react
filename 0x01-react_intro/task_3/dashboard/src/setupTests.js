82596e0fc1b94ea11c49f1677777b8637abfc255
dmuchai <dmmuchai@gmail.com>
2025-05-14 16:45:27 +0300

    Task 7. Create React tests

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,5 +1,7 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'enzyme-to-json';
+import { TextEncoder, TextDecoder } from 'util';
 
 configure({ adapter: new Adapter() });
 
