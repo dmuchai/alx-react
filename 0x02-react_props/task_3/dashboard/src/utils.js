1656c957f8ca5de1e2b6543815255cd1d0269872
dmuchai <dmmuchai@gmail.com>
2025-05-19 23:31:45 +0300

    Task 0. Basic components

---
+++ 0x02-react_props/task_3/dashboard/src/utils.js
@@ -0,0 +1,5 @@
+export const getFullYear = () => new Date().getFullYear();
+
+export const getFooterCopy = (isIndex) => (isIndex ? "ALX" : "ALX main dashboard");
+
+export const getLatestNotification = () => "<strong>Urgent requirement</strong> - complete by EOD";
