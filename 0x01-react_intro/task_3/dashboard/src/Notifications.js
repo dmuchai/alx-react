860e4f2cabc3f45d9ade5b67b5b735744ab345bd
Dennis Muniu Muchai <dmmuchai@gmail.com>
2025-05-23 00:32:31 +0300

    Remove node_modules from repo

--- 0x01-react_intro/task_3/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_3/dashboard/src/Notifications.js
@@ -3,38 +3,28 @@ import './Notifications.css';
 import closeIcon from './close-icon.png';
 import { getLatestNotification } from './utils';
 
-function Notifications() {
-  const handleClick = () => {
-    console.log('Close button has been clicked');
-  };
-
-  return (
-    <div className="Notifications">
-      <button
-        style={{
-          position: 'absolute',
-          top: '10px',
-          right: '10px',
-          background: 'transparent',
-          border: 'none',
-          cursor: 'pointer',
-        }}
-        aria-label="Close"
-        onClick={handleClick}
-      >
-        <img src={closeIcon} alt="close icon" style={{ width: '10px', height: '10px' }} />
-      </button>
-      <p>Here is the list of notifications</p>
-      <ul>
-        <li data-priority="default">New course available</li>
-        <li data-priority="urgent">New resume available</li>
-        <li
-          data-priority="urgent"
-          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
-        ></li>
-      </ul>
-    </div>
-  );
-}
+const Notifications = () => {
+	return (
+		<div className='Notifications'>
+			<p style={{ display: 'inline', marginRight: '80%' }}>
+				Here is the list of notifications
+			</p>
+			<button
+				aria-label='Close'
+				onClick={console.log('Close button has been clicked')}
+			>
+				<img style={{ display: 'inline' }} src={closeIcon} alt='Close' />
+			</button>
+			<ul>
+				<li data='default'>New course available</li>
+				<li data='urgent'>New resume available</li>
+				<li
+					data='urgent'
+					dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+				></li>
+			</ul>
+		</div>
+	);
+};
 
 export default Notifications;
