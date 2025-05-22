0a8ff7ef0e99946ed7f692e586b9c7413ccdede9
Dennis Muniu Muchai <dmmuchai@gmail.com>
2025-05-22 23:01:03 +0300

    React app intro

--- 0x01-react_intro/task_4/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_4/dashboard/src/setupTests.js
@@ -1,4 +1,11 @@
+// src/setupTests.js
 import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 configure({ adapter: new Adapter() });
+
+// Polyfill TextEncoder/Decoder for Jest
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
