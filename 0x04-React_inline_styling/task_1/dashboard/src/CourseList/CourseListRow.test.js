597131f6d33f8597e58abf4f20beb97492086822
Dennis Muniu Muchai <dmmuchai@gmail.com>
2025-05-27 22:07:19 +0300

    Task 1. install Aphrodite.

--- 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.test.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.test.js
@@ -1,53 +1,60 @@
-import React from "react";
-import CourseListRow from "./CourseListRow";
-import { shallow } from "enzyme";
+import React from 'react';
+import { shallow } from 'enzyme';
+import CourseListRow from './CourseListRow';
+import { StyleSheetTestUtils } from 'aphrodite';
 
-describe("Course List Row component test", () => {
-  it("should render without crashing", () => {
-    const wrapper = shallow(<CourseListRow textFirstCell="test" />);
-    expect(wrapper.exists()).toBe(true);
+describe('<CourseListRow />', () => {
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
   });
 
-  it("should render one cell with colspan = 2 and header style when textSecondCell is null", () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<CourseListRow textFirstCell='test' />);
+    expect(wrapper.exists());
+  });
+
+  it('renders one cell', () => {
     const wrapper = shallow(
-      <CourseListRow isHeader={true} textFirstCell="test" textSecondCell={null} />
+      <CourseListRow isHeader={true} textFirstCell='test' />
     );
-
-    const tr = wrapper.find("tr");
-    expect(tr.prop("style")).toEqual({ backgroundColor: "#deb5b545" });
-
-    const th = wrapper.find("th");
+    const th = wrapper.find('th');
+    expect(wrapper.exists());
+    expect(th.exists());
     expect(th).toHaveLength(1);
-    expect(th.prop("colSpan")).toBe(2);
-    expect(th.text()).toBe("test");
+    expect(th.prop('colSpan')).toEqual('2');
   });
 
-  it("should render two td cells and row style when isHeader is false", () => {
+  it('renders two cells', () => {
     const wrapper = shallow(
-      <CourseListRow isHeader={false} textFirstCell="test" textSecondCell="data" />
+      <CourseListRow
+        isHeader={true}
+        textFirstCell='test'
+        textSecondCell='second'
+      />
     );
-
-    const tr = wrapper.find("tr");
-    expect(tr.prop("style")).toEqual({ backgroundColor: "#f5f5f5ab" });
-
-    const td = wrapper.find("td");
-    expect(td).toHaveLength(2);
-    expect(td.at(0).text()).toBe("test");
-    expect(td.at(1).text()).toBe("data");
-  });
-
-  it("should render two th cells and header style when isHeader is true and both cells are provided", () => {
-    const wrapper = shallow(
-      <CourseListRow isHeader={true} textFirstCell="Header1" textSecondCell="Header2" />
-    );
-
-    const tr = wrapper.find("tr");
-    expect(tr.prop("style")).toEqual({ backgroundColor: "#deb5b545" });
-
-    const th = wrapper.find("th");
+    const th = wrapper.find('th');
+    expect(wrapper.exists());
+    expect(th.exists());
     expect(th).toHaveLength(2);
-    expect(th.at(0).text()).toBe("Header1");
-    expect(th.at(1).text()).toBe("Header2");
+    expect(th.first().text()).toEqual('test');
+    expect(th.at(1).text()).toEqual('second');
+  });
+
+  it('renders two td', () => {
+    const wrapper = shallow(
+      <CourseListRow
+        isHeader={false}
+        textFirstCell='test'
+        textSecondCell='second'
+      />
+    );
+    const tr = wrapper.find('tr');
+    expect(wrapper.exists());
+    expect(tr.exists());
+    expect(tr).toHaveLength(1);
+    expect(tr.children('td')).toHaveLength(2);
   });
 });
-
--- 0x04-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.test.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import NotificationItem from './NotificationItem';
+import Notifications from './Notifications';
+import { getLatestNotification } from '../utils/utils';
 import { StyleSheetTestUtils } from 'aphrodite';
 
-describe('<NotificationItem />', () => {
+describe('<Notification />', () => {
   beforeAll(() => {
     StyleSheetTestUtils.suppressStyleInjection();
   });
@@ -12,30 +13,166 @@ describe('<NotificationItem />', () => {
   });
 
   it('render without crashing', () => {
-    const wrapper = shallow(<NotificationItem />);
+    const wrapper = shallow(<Notifications />);
     expect(wrapper.exists());
   });
 
-  it('renders type and value props', () => {
-    const wrapper = shallow(<NotificationItem type='default' value='test' />);
-    const li = wrapper.find('li');
-    expect(wrapper.exists());
-    expect(li.exists());
-    expect(li).toHaveLength(1);
-    expect(li.text()).toEqual('test');
-    expect(li.prop('data-notification-type')).toEqual('default');
+  it('Notification Item with html', () => {
+    const wrapper = shallow(<Notifications displayDrawer />);
+    const nItem = wrapper.find('NotificationItem');
+    expect(nItem).toBeDefined();
+  });
+  it('Notification with displayDrawer false', () => {
+    const wrapper = shallow(<Notifications />);
+    const dNoti = wrapper.find('div.Notifications');
+    expect(dNoti).toHaveLength(0);
+  });
+});
+
+describe('listNotifications with values', () => {
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
   });
 
-  it('renders html prop', () => {
-    const text = 'Here is the list of notifications';
+  let latestNotification = undefined;
+  let listNotifications = undefined;
+
+  beforeEach(() => {
+    latestNotification = getLatestNotification();
+    listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: latestNotification } },
+    ];
+  });
+
+  it('values', () => {
     const wrapper = shallow(
-      <NotificationItem html={{ __html: '<u>test</u>' }} />
+      <Notifications displayDrawer listNotifications={listNotifications} />
     );
-    const li = wrapper.find('li');
     expect(wrapper.exists());
-    expect(li.exists());
-    expect(li.html()).toEqual(
-      '<li data-notification-type="default"><u>test</u></li>'
+    const nItem = wrapper.find('NotificationItem');
+    expect(nItem).toBeDefined();
+    expect(nItem).toHaveLength(3);
+    expect(nItem.at(0).html()).toEqual(
+      '<li data-notification-type="default">New course available</li>'
     );
+    expect(nItem.at(1).html()).toEqual(
+      '<li data-notification-type="urgent">New resume available</li>'
+    );
+    expect(nItem.at(2).html()).toEqual(
+      `<li data-notification-type="urgent">${latestNotification}</li>`
+    );
+  });
+});
+
+describe('listNotifications without values', () => {
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
+  let listNotifications = undefined;
+  beforeEach(() => {
+    listNotifications = [];
+  });
+
+  it('empty', () => {
+    const wrapper = shallow(
+      <Notifications displayDrawer listNotifications={listNotifications} />
+    );
+    expect(wrapper.exists());
+    const nItem = wrapper.find('NotificationItem');
+    expect(nItem).toHaveLength(1);
+    expect(nItem.html()).toEqual(
+      '<li data-notification-type="default">No new notification for now</li>'
+    );
+  });
+
+  it('without listNotifications', () => {
+    const wrapper = shallow(<Notifications displayDrawer />);
+    const nItem = wrapper.find('NotificationItem');
+    expect(nItem).toHaveLength(1);
+    expect(nItem.html()).toEqual(
+      '<li data-notification-type="default">No new notification for now</li>'
+    );
+  });
+});
+
+describe('markAsRead', () => {
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
+  it('console.log', () => {
+    const wrapper = shallow(<Notifications displayDrawer />);
+    console.log = jest.fn();
+    const instance = wrapper.instance();
+    const id = 0;
+    instance.markAsRead(id);
+    expect(console.log).toHaveBeenCalledWith(
+      `Notification ${id} has been marked as read`
+    );
+    jest.restoreAllMocks();
+  });
+});
+
+describe('updating the props of the component', () => {
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
+  it('with the same list, the component doesn’t rerender', () => {
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+    ];
+    const wrapper = shallow(
+      <Notifications displayDrawer listNotifications={listNotifications} />
+    );
+    const shouldComponentUpdate = jest.spyOn(
+      Notifications.prototype,
+      'shouldComponentUpdate'
+    );
+    wrapper.setProps({ listNotifications: listNotifications });
+    expect(shouldComponentUpdate).toHaveBeenCalled();
+    expect(shouldComponentUpdate).toHaveLastReturnedWith(false);
+    jest.restoreAllMocks();
+  });
+
+  it('with a longer list, the component does rerender', () => {
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+    ];
+    let latestNotification;
+    const listNotifications2 = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: latestNotification } },
+    ];
+    console.log(listNotifications);
+    const wrapper = shallow(
+      <Notifications displayDrawer listNotifications={listNotifications} />
+    );
+    const shouldComponentUpdate = jest.spyOn(
+      Notifications.prototype,
+      'shouldComponentUpdate'
+    );
+    wrapper.setProps({ listNotifications: listNotifications2 });
+    expect(shouldComponentUpdate).toHaveBeenCalled();
+    expect(shouldComponentUpdate).toHaveLastReturnedWith(true);
+    jest.restoreAllMocks();
   });
 });
